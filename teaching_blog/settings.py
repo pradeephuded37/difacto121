2678cbe021a2830940213bee5fb0199dbc7e0aba
difactorobotics <pradeephuded37@gmail.com>
2022-06-08 20:44:37 +0530

    third commit

--- teaching_blog/settings.py
+++ teaching_blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '!l)42+-o1d&+jm)x)!uzbnt@rngrs0=@w@_xip@vj4jp!mil5f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','difacto.herokuapp.com']
 
 
 # Application definition
